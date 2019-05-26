2b9180e7b78037efa7f601285c9925894cacffae
Ник Чередник <777archie.nik777@gmail.com>
2019-05-26 19:53:23 +0300

    v5.3

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="robots" content="index, follow">
+    <title>PUBG TOUR - зарегестриваться</title>
+
+    <meta name="description" content="Круто играешь в Pubg Mobile? Если это так, то скорее зарегестрируйся. Сдесь будут проходить жесточайшие турниры со ставками. И лишь самый сильный сможет победить." />
+    <meta name="keywords" content=" ,Сквады ,Игра пабг, Турнир, Pubg Tour, pubg, пабг, PlayerUnknown’s Battlegrounds, игровые ставки, соревнования, денежные ставки, пабг регистрация, плэйер анкнаунс баттле граунд">
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
@@ -74,39 +79,6 @@
             </div>
         </div>
     </div>
-    <script>
-    (function( $ ){
-
-    var $body;
-
-    $(document).ready(function(){
-      $body = $('body');
-
-      $body
-        .find('.user-phone').each(function(){
-            $(this).mask("+38 (999) 999-99-99", {autoсlear: false});
-        });
-
-      $body.on('keyup','.user-phone',function(){
-        var phone = $(this),
-            phoneVal = phone.val(),
-            form = $(this).parents('form');
-
-        if ( (phoneVal.indexOf("_") != -1) || phoneVal == '' ) {
-          form.find('.btn_submit').attr('disabled',true);
-        } else {
-          form.find('.btn_submit').removeAttr('disabled');
-        }
-      });
-
-    });
-
-  })( jQuery );</script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script src="jquery.maskedinput.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-    </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
